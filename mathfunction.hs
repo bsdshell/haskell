132a35cafb2e4592cf71efaed46d80b977f5b71d
bsdshell <000b000@gmail.com>
2014-07-08 01:56:10 -0700

    add

--- mathfunction.hs
+++ mathfunction.hs
@@ -1,7 +1,7 @@
 --Factorial function
-fac::Int->Int
-fac 0 = 1
-fac n = n * fac(n-1)
+factorial::Int->Int
+factorial 0 = 1
+factorial n = n * factorial(n-1)
 
 --Sum all the integer in a list
 sumlist::[Int]->Int
@@ -9,5 +9,10 @@ sumlist [] = 0
 sumlist (x:xs) = x+sumlist(xs) 
 
 --Add two vectors
-addVector::(Num a)=>(a, a)->(a, a)->(a, a)
-addVector (a, b) (c, d) = (a + c, b + d)
+addvector::(Num a)=>(a, a)->(a, a)->(a, a)
+addvector (a, b) (c, d) = (a + c, b + d)
+
+main = do
+        print (factorial(5))
+        print (addvector (1,2) (2,3))
+        print (sumlist [1, 2, 3])
