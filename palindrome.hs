6c8e838afb38beb05060206907a8b23a1fdd8ef9
bsdshell <bsdshell@gmail.com>
2014-07-09 04:07:22 -0400

    add

--- palindrome.hs
+++ palindrome.hs
@@ -4,8 +4,4 @@ isPalindrome [x,y] = x == y
 isPalindrome (x:xs) = x == (last xs) && isPalindrome(init(xs))
 
 main = do
-        print ("a="++ show(isPalindrome("a")))
-        print ("aa="++ show(isPalindrome("aa")))
-        print ("ab="++ show(isPalindrome("ab")))
-        print ("aba="++ show(isPalindrome("aba")))
         print ("aba="++ show(isPalindrome("aaa")))
