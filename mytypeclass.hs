c490e4441968eade26cbfb55d9279bf5a8a5bbe5
bsdshell <000b000@gmail.com>
2015-01-24 01:52:24 -0800

    Add more typeclass example

--- mytypeclass.hs
+++ mytypeclass.hs
@@ -1,13 +1,23 @@
 --Typeclass in Haskell
 --Shape is similar to interface in Java
 --Circle and Rectangle are similar to class in Java
+data Point = Point Float Float deriving(Show)
 data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+
+data NewShape = NewCircle Point Float | NewRectangle Point Point deriving (Show)
+
 
 area::Shape->Float
 area(Circle _ _ r) = pi*r^2
 area(Rectangle topx topy bottomx bottomy) = abs(bottomy - topy)*abs(bottomx - topx)
 
+newarea::NewShape->Float
+newarea(NewCircle _ r) = pi*r^2 
+newarea(NewRectangle (Point topx topy) (Point bottomx bottomy)) = abs(bottomy - topy)*abs(bottomx - topx)
+
 main = do 
         print("Typeclass in Haskell")
         print(area $ Circle 3 4 5)
         print(area $ Rectangle 3 4 5 8)
+        print(newarea $ NewCircle (Point 1 4) 3)
+        print(newarea $ NewRectangle (Point 1 4) (Point 2 7))
