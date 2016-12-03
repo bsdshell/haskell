1a104af5db73e797c5ba2db3f78eeab7a4a5019d
bsdshell <monosurface@gmail.com>
2016-12-03 12:19:54 -0800

    improve it again

--- parsecvs.hs
+++ parsecvs.hs
@@ -18,8 +18,8 @@ isStr s = if s !! 0 == '"' && s !! (length s -1) == '"' then True else False
 pad::Int->String
 pad n = foldr (++) "" $ replicate n "-"
 
-trimQ::String->String
-trimQ s = tail $ drop 1 s 
+trim::String->String
+trim s = tail . init $ s 
 
 maxlen::[String]->Int
 maxlen [] = 0
@@ -32,8 +32,8 @@ sub = \x -> subRegex (mkRegex "\\\\r\\\\n") x " "
 
 main = do
         list <- readFileToList fname
-        let llist= map(\x -> map(\y -> if isStr $ head y then sub $ head y else head y) (capture x) ) list
-        let padlist   = map(\l -> map(\x -> if isStr x then (trimQ x ++ pad (maxlen(l) - length(trimQ x) )) else pad (maxlen(l) - length x) ++ x ) l) llist 
-        let splist  = map(\l -> map(\x -> x ++ " ") l) padlist 
-        let flist = map(\l -> foldr(++) [] l) splist 
+        let llist = map(\l -> map(\x -> if isStr $ head x then sub $ head x else head x) (capture l) ) list
+        let plist = map(\l -> map(\x -> if isStr x then (trim x ++ pad(maxlen(l) - length (trim x) )) else pad(maxlen(l) - length x) ++ x ) l) llist
+        let slist = map(\l -> map(\x -> x ++ " ") l) plist
+        let flist = map(\l -> foldr(++) [] l) slist
         writeToFile outname flist 
