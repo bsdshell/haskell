efb4f13464be4f1933972a278d4416b5f77e11b6
guys <adfasdfasd@asdfadf.com>
2017-09-02 13:17:24 -0700

    ad

--- renameAll.hs
+++ renameAll.hs
@@ -1,15 +1,38 @@
 import Data.Char 
+import Control.Monad
 import System.IO
 import System.Environment
 import System.Directory
 
+-- rename -o dir  KK_
 oldDir = "/Users/longshu/try/test1"
 newDir = "/Users/longshu/try/test1"
-prefix = "KKK_"
 main = do 
-        print "Hello World"
-        allFiles <- listDirectory oldDir
-        mapM print allFiles
-        mapM_(\x -> renameFile (oldDir ++ "/" ++ x)  (oldDir ++ "/" ++ prefix ++ x)) allFiles 
-        print "done!"
+        print "rename -o dir file_prefix"
+        print "rename -o /dir file_prefix"
+        argList <- getArgs 
+        print argList
 
+        let option = head argList
+        let currDir = getCurrentDirectory
+        contents <- listDirectory oldDir
+        fileList <- filterM(\x -> doesFileExist $ oldDir ++ "/" ++ x) contents 
+        mapM print fileList 
+        print ("Option=" ++ option)
+
+
+        let len = length argList
+        case len of
+            1 -> do
+                let argList = 
+                mapM_(\x -> renameFile (oldDir ++ "/" ++ x)  (newDir ++ "/" ++ dir ++ "/" ++ prefix ++ x)) fileList 
+            3 -> do
+                case option of
+                        "-o" -> do
+                            let dir = argList !! 1
+                            let prefix = argList !! 2
+                            doesExist <- doesDirectoryExist (newDir ++ "/" ++ dir)
+                            if doesExist == False then createDirectory (newDir ++ "/" ++ dir) else print "dir exist" 
+                            mapM_(\x -> renameFile (oldDir ++ "/" ++ x)  (newDir ++ "/" ++ dir ++ "/" ++ prefix ++ x)) fileList 
+                        _  -> putStrLn "done!"
+            _ -> putStrLn "len = "
