c49e49138b016f2a492e441e0e25e8690421337b
bsdshell <monosurface@gmail.com>
2017-09-23 14:29:01 -0700

    add

--- command.hs
+++ command.hs
@@ -1,25 +1,40 @@
+{-# OPTIONS_GHC -Wall #-}
 import System.Process
-import Data.Char 
-import Control.Monad
 import System.IO
 import System.Environment
-import System.Directory
-import AronModule
+
+f::[String] -> IO () 
+f x = case x of 
+        [op, a1] -> case op of
+                         "zip" -> createProcess (proc "/usr/bin/zip"    ["-r", (a1 ++ ".zip"), a1]){ cwd = Just "." } >>= \_ -> return () 
+                         "gz"  -> createProcess (proc "/usr/bin/gzip"   [a1]){ cwd = Just "." } >>= \_ -> return () 
+                         "tar" -> createProcess (proc "/usr/bin/tar"    ["-czvf", (a1 ++ ".tar.gz"), a1]){ cwd = Just "." } >>= \_ -> return () 
+                         "utar"-> createProcess (proc "/usr/bin/tar"    ["-xzvf", a1]){ cwd = Just "." } >>= \_ -> return () 
+                         "uzip"-> createProcess (proc "/usr/bin/unzip"  [a1]){ cwd = Just "." } >>= \_ -> return () 
+                         "ugz" -> createProcess (proc "/usr/bin/gunzip" [a1]){ cwd = Just "." } >>= \_ -> return () 
+--                         "grep"-> createProcess (proc "/usr/bin/grep"   ["-Hnis", "--color", "--include=\"*.hs\"", a1, "."]){ cwd = Just ".", std_out=CreatePipe } 
+--                                   >>= \out -> case out of 
+--                                                 (_,_,Just hout,_) -> hGetContents hout >>= print 
+--                                                 _                 -> return () 
+
+                         "grep"-> createProcess (proc "/bin/ls" []){ cwd = Just ".", std_out=CreatePipe } 
+                                   >>= \out -> case out of 
+                                                 (_,_,Just hout,_) -> hGetContents hout >>= print 
+                                                 _                 -> return () 
+                         _     -> return () 
+        [op, a1, a2] -> do
+            case op of
+                 "zip" -> createProcess (proc "/usr/bin/zip" ["-r", a1, a2]){ cwd = Just "." } >>= \_ -> return () 
+                 "grep" -> do
+                           (_,Just hout,_,_) <- createProcess (proc "/usr/bin/grep" ["--color", "--include=\"*.hs\"", "-Hnris", a1]){ cwd = Just "." } 
+                           out <- hGetContents hout
+                           print out
+                           return () 
+                 _     -> print "empty op"
+        _ -> return ()
 
 main =  do
-        getArgs >>= \arg -> case arg of 
-                                [op,a1] -> case op of
-                                                "zip" -> createProcess (proc "/usr/bin/zip" ["-r", (a1 ++ ".zip"), a1]){ cwd = Just "." } >>= \x  -> case x of
-                                                                                                                                                    (_,Just hout,_,_) -> hGetContents hout >>= print 
-                                                                                                                                                    _ -> return ()
-                                                "uzip" -> createProcess (proc "/usr/bin/unzip" [a1]){ cwd = Just "." } >>= \x -> case x of
-                                                                                                                                 (_,Just hout,_,_) -> hGetContents hout >>= print 
-                                                                                                                                 _ -> return ()
-                                                _ -> putStrLn ("Invalid Op " ++ arg) 
-                                [op,a1,a2] -> case op of 
-                                                   "zip" -> createProcess (proc "/usr/bin/zip" ["-r", a1, a2]){ cwd = Just "." } >>= \x  -> case x of
-                                                                                                                                        (_,Just hout,_,_) -> hGetContents hout >>= print 
-                                                                                                                                        _ -> return ()
-                                                   _ -> print "invalid option"     
-
-                                _ -> return () 
+        argList <- getArgs
+        s <- f argList
+        print s
+        return ()
